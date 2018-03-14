a53529cc909065aa6cd6483dc48298a662f53dd0
Nick Meldrum <nick@nickmeldrum.com>
2018-03-14 17:05:30 +0000

    okay fixed my rubbish abstraction

--- test-apis.js
+++ test-apis.js
@@ -1,16 +1,5 @@
 const waitroseApi = require('./waitrose-api')
 
-const main = () => {
-  const searchTerm = process.argv[2];
+const searchTerm = process.argv[2];
 
-  waitroseApi.login()
-    .then(() => {
-      return waitroseApi.token()
-        .then(data => {
-          console.log('umwat', data)
-        })
-    })
-    .catch(err => console.error(err))
-};
-
-main();
+waitroseApi.login().then(waitroseApi.token).then(console.log).catch(console.error)
--- waitrose-api/api-call.js
+++ waitrose-api/api-call.js
@@ -1,26 +1,35 @@
 const http = require('https')
 
 module.exports = options => {
+  if (options.body) options.bodyString = JSON.stringify(options.body)
+
   const opts = {
     host: 'www.waitrose.com',
     path: options.path,
     method: options.method,
+    headers: {
+      'Content-Type': 'application/json',
+    }, 
   }
+
+  if (options.body) opts.headers['Content-Length'] = Buffer.byteLength(options.bodyString)
 
   return new Promise((resolve, reject) => {
     const req = http.request(opts, res => {
-      const body = ''
+      let body = ''
       res.setEncoding('utf8')
       res.on('readable', function() { body += this.read() || ''})
       res.on('end', () => {
-        if (body.length) resolve(JSON.parse(body))
-        else resolve()
+        if (res.statusCode > 399) reject(res.statusCode)
+        else {
+          if (body.length) resolve(JSON.parse(body))
+          else resolve()
+        }
       })
     })
 
     req.on('error', e => reject(e))
-    if (options.body) req.write(JSON.stringify(options.body))
+    if (options.body) req.write(options.bodyString)
     req.end()
-  }
-)}
-
+  })
+}
