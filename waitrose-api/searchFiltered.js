8818d1385bb42a063f1fc3c539a434812409415a
Mary Stocks <Mary.Stocks@waitrose.co.uk>
2018-03-15 13:11:06 +0000

    added selectable filters

--- waitrose-api/searchFiltered.js
+++ waitrose-api/searchFiltered.js
@@ -2,8 +2,26 @@
 
 const api = require('./api-call')
 
-module.exports = (customerId, orderId, searchTerm) =>
-  api.call({
+module.exports = (customerId, orderId, searchTerm, filterKey) => {
+  let filterTags
+  switch(filterKey) {
+    case 'gluten':
+      filterTags = [{id:"suitableforthoseavoidingglutenfilter",value:"Suitable_for_those_avoiding_gluten"}]
+      break
+    case 'milk':
+      filterTags = [{id:"suitableforthoseavoidingmilkfilter",value:"Suitable_for_those_avoiding_milk"}]
+      break
+    case 'nuts':
+      filterTags = [{id:"suitableforthoseavoidingnutsfilter",value:"Suitable_for_those_avoiding_nuts"}]
+      break
+    case 'egg':
+      filterTags = [{id:"suitableforthoseavoidingeggfilter",value:"Suitable_for_those_avoiding_egg"}]
+      break
+    case 'soya':
+      filterTags = [{id:"suitableforthoseavoidingsoyafilter",value:"Suitable_for_those_avoiding_soya"}]
+      break
+  }
+  return api.call({
     path: `/content-prod/v2/cms/publish/productcontent/search/${customerId}?clientType=WEB_APP`,
     method: 'POST',
     body: {
@@ -13,9 +31,10 @@ module.exports = (customerId, orderId, searchTerm) =>
           searchTerm,
           sortBy: 'RELEVANCE',
           searchTags: [],
-          filterTags:[{id:"suitableforthoseavoidingglutenfilter",value:"Suitable_for_those_avoiding_gluten"}],
+          filterTags,
           orderId,
         },
       },
     },
   })
+}
